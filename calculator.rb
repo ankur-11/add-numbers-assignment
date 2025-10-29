934dc58aecedc65fe08fa61a5d3b72ea6be2fa53
Ankur Gupta <ankur11.gpt@gmail.com>
2025-10-29 23:50:02 +0530

    addtiom of exception for negative numbers, test case passing

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,10 @@
 class Calculator
     def add(string_of_numbers)
+        if have_negative_numbers?(string_of_numbers)
+            negative_numbers = string_of_numbers.scan(/-?\d+/).select { |n| n.to_i < 0 }
+            raise "negative numbers not allowed #{negative_numbers.join(", ")}"
+        end
+
         string_of_numbers = escape_custom_delimiter(string_of_numbers)
         string_of_numbers = escape_new_line_delimiter(string_of_numbers)
         numbers_array = string_of_numbers.split(",")
@@ -18,5 +23,9 @@ class Calculator
         delimiter = string_of_numbers[2]
         string_of_numbers = string_of_numbers[4..-1]
         string_of_numbers.gsub(delimiter, ",")
+    end
+
+    def have_negative_numbers?(string_of_numbers)
+        string_of_numbers.include?("-")
     end
 end
