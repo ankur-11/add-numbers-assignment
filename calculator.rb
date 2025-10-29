cb4258aee9edb6201f5edbc79624ba91d4da5d04
Ankur Gupta <ankur11.gpt@gmail.com>
2025-10-29 23:34:28 +0530

    method for escaping new line delimeter, specs passing

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,11 @@
 class Calculator
     def add(string_of_numbers)
-        numbers = string_of_numbers.split(",")
-        numbers.map(&:to_i).sum
+        string_of_numbers = escape_new_line_delimiter(string_of_numbers)
+        numbers_array = string_of_numbers.split(",")
+        numbers_array.map(&:to_i).sum
+    end
+
+    def escape_new_line_delimiter(string_of_numbers)
+        string_of_numbers.gsub("\n", ",")
     end
 end
